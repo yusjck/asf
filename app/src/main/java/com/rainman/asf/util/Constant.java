b8b62f53bbeef21b838dd661daa07380cc04b8e8
yusjck <yusjck@gmail.com>
2021-04-21 15:32:58 +0800

    更换脚本下载服务器地址

--- app/src/main/java/com/rainman/asf/util/Constant.java
+++ app/src/main/java/com/rainman/asf/util/Constant.java
@@ -2,6 +2,6 @@ package com.rainman.asf.util;
 
 public class Constant {
     public static final String SCRIPT_RECORD_FILE = "scripts.json";
-    public static final String SCRIPT_LIST_URL = "http://asf.nana3.cn/getlist.php";
+    public static final String SCRIPT_LIST_URL = "https://hero.ddnsgeek.com/asf/getlist.php";
     public static final String SCRIPT_MANIFEST_FILE = "manifest.xml";
 }
